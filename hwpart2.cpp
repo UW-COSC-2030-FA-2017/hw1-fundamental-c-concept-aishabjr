0160c5347169aa3ca2f0c5c4b8607abf24d6dff8
Aisha Balogun <nbalogun@uwyo.edu>
2017-09-17 21:30:39 -0600

    Update hwpart2.cpp

--- hwpart2.cpp
+++ hwpart2.cpp
@@ -5,10 +5,10 @@ void recursiveFunction(int time)
 {
 	cout << "Step 3: Water is not boiled yet, increase gas" << endl;
 	cout << "Step 4: Wait for 5 more minutes " << endl;
-	time++;
+	time++; //the counter is incremented
 	if (time < 5)
 	{
-		recursiveFunction(time);
+		recursiveFunction(time); //the function calls on itself 
 	}
 }
 
@@ -19,8 +19,8 @@ int main()
 	cout << "Steps to cook Pasta" << endl;
 	cout << "Step 1: Put water on Fire" << endl;
 	cout << "Step 2: Wait for 5 mins" << endl;
-	recursiveFunction(time);
-	cout << "Step 5: Water is boiled now, Add Pasta now" << endl;
+	recursiveFunction(time); //main calls on the function
+	cout << "Step 5: Water is boiled now, Add Pasta now" << endl; 
 
 	return 0;
 }
